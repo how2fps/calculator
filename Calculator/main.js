9384ab081aeee7d79f244edfe598781878f346a0
how2fps <68309914+how2fps@users.noreply.github.com>
2020-10-07 00:45:49 +0800

    Add files via upload

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,12 +1,21 @@
-const keys = document.querySelector('.calculator-buttons')
-const display = document.querySelector('.calculator-screen')
-keys.addEventListener('click', event => {
-    if (!event.target.closest('button'))
-        return
-    const key = event.target
-    console.log(key.textContent)
-    const keyValue = key.textContent
-    displayValue = display.textContent
-    console.log(display.textContent)
-}
-)
+const keys = document.querySelector(".calculator-buttons");
+const display = document.querySelector(".calculator-screen");
+keys.addEventListener("click", (event) => {
+  if (!event.target.closest("button")) return;
+  const key = event.target;
+  const keyValue = key.textContent;
+  displayValue = display.textContent;
+
+  if (key.classList.contains("number")) {
+    if (displayValue === "0") {
+      //Is this is a number key?
+      display.textContent = keyValue;
+    } else {
+      display.textContent = displayValue + keyValue;
+    }
+  }
+  //Is this an operator key?
+  if (key.classList.contains("operator")) {
+    console.log(key);
+  }
+});
